5450052b676791189bec06bd9753190848ee9d31
mut_mut <anhialo.mut@gmail.com>
2024-01-01 20:08:31 +0200

    js-auth - 2 end

--- src/route/auth.js
+++ src/route/auth.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  password: 123,
+  rol: 1,
+})
+
 router.get('/signup', function (req, res) {
   return res.render('signup', {
     name: 'signup',
@@ -28,6 +34,34 @@ router.get('/signup', function (req, res) {
       ],
     },
   })
+})
+
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body
+
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: 'Помилка. Обов`язкові поля відсутні!',
+    })
+  }
+
+  try {
+    User.create({
+      email,
+      password,
+      role,
+    })
+
+    return res.status(200).json({
+      message: 'Користувач успішно зареєсрований',
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Помилка створення користувача',
+    })
+  }
 })
 
 module.exports = router
